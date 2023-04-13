2050418af18e29a8927dd718e8745ade13583457
userparkms <129916999+userparkms@users.noreply.github.com>
2023-04-13 22:08:52 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,20 +2,30 @@
 # return sum
 def plus(x, y):
     return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
 
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus")
+        print("0: exit 1: keep")
         check = int(input())
         if check == 1:
             print("First Number")
             x = int(input())
             print("Second Number")
             y = int(input())
-            print("answer : ", plus(x,y))
+            print("plus : ", plus(x,y))
+            print("sub : ", sub(x,y))
+            print("mul : ", mul(x,y))
+            print("div : ", div(x,y))
+
         elif check > 1:
             print("Unsupported")
         else:
